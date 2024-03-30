9a13d64c0debba439a00914003c2f5927fa8f1cf
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-03-30 22:23:15 +0200

    feat(migrations) all tables added through migrations

---
+++ server/database/migrations/2024_03_30_195802_create_invitations_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('from');
+            $table->unsignedBigInteger('to');
+            $table->foreign('from')->references('id')->on('users');
+            $table->foreign('to')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('invitations');
+    }
+};
---
+++ server/database/migrations/2024_03_30_200610_create_stations_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    public function up(): void
+    {
+        Schema::create('stations', function (Blueprint $table) {
+            $table->id();
+            $table->string('location');
+            $table->string('name');
+            $table->string('image');
+            $table->string('status');
+            $table->double('longitude')->nullable();
+            $table->double('latitude')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('facilities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('station_facilities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('station_id');
+            $table->unsignedBigInteger('facility_id');
+            $table->foreign('station_id')->references('id')->on('stations');
+            $table->foreign('facility_id')->references('id')->on('facilities');
+            $table->timestamps();
+        });
+
+        Schema::create('station_operating_hours', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('station_id');
+            $table->dateTime('from');
+            $table->dateTime('to');
+            $table->foreign('station_id')->references('id')->on('stations');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('stations');
+    }
+};
