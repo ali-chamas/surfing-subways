c7a3c4b70de301cd14c9ad25aa3bfe32f0c08e97
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-04-01 01:23:00 +0300

    add review btn done// installed axios

--- client/src/pages/singleStation/components/AddReviewPopup.jsx
+++ client/src/pages/singleStation/components/AddReviewPopup.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import RatingStars from "../../../common/components/RatingStars";
+import React, { useState } from 'react'
+import axios from "axios";
 
-const AddReviewPopup = ({review, setOpenAddReviewPopup}) => {
+const AddReviewPopup = ({setOpenAddReviewPopup}) => {
+
+    const [reviews, setReviews] = useState([{message : '', rating: ''}])
+
   return (
     <div className='popup'>
       <div className='flex column align-center popup-child bg-default border-radius gap p'>
@@ -9,18 +12,44 @@ const AddReviewPopup = ({review, setOpenAddReviewPopup}) => {
         <div className='flex row justify-between w-full bg-black border-radius p'>
             <div className='flex column center'>
                 <h3 className="text-primary letter-spacing">REVIEW</h3>
-              <textarea className='bg-default text-white' placeholder='Enter review description...' name="" id="" cols="30" rows="10">
+              <textarea className='bg-default text-white' placeholder='Enter review description...' name="" id="" cols="30" rows="10" onChange={(e)=>{
+                setReviews({
+                    ...reviews,
+                    message: e.target.value,
+                })
+              }}>
               </textarea>
             </div>
             <div className="flex column align-center justify-between">
                 <div className='flex column center'>
                 <h3 className="text-primary letter-spacing">RATING</h3>
                 <input className='bg-default text-white' placeholder='1 / 5' type='number'min={1}
-                max={5} name="" id="" />
+                max={5} name="" id="" onChange={(e)=>{
+                    setReviews({
+                        ...reviews,
+                        rating: e.target.value,
+                    })
+                }}/>
                 </div>
               
-                <p onClick={()=>{
-                // setOpenAddReviewPopup(true);
+                <p onClick={async()=>{
+                    try{
+                        const {message , rating} = reviews
+
+                        const response = await axios.post(
+                            "",
+                            {
+                                message,
+                                rating,
+                            }
+                        );
+                        if(response.data.status === "success"){
+                        setOpenAddReviewPopup(false);
+                    }
+                    
+                    } catch(error){  
+                        console.error(error);
+                    }
                 }} className='add-rev cursor-pointer bg-secondary text-black bold border-radius'>ADD REVIEW</p>
             </div>
             
