87369dd1a60e0c49ca6bdd9b1d4f6b1937ecd24f
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-03-31 17:35:32 +0300

    review pop 70% done

--- client/src/pages/singleStation/components/ReviewsPopup.jsx
+++ client/src/pages/singleStation/components/ReviewsPopup.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-const ReviewsPopup = () => {
+import Reviews from './Reviews'
+
+const ReviewsPopup = ({review, setOpenReviewPopup}) => {
+
+  
+
   return (
-    <div>
+    <div className='popup'>
       
+      <div className='flex column align-center popup-child bg-primary gap p'>
+        <div className='flex row gap align-center'>
+          <h1 className='primary-text'>REVIEWS</h1>
+          <i className="icon-review"></i>
+        </div>
+        <div className='flex column w-full gap'>
+        {review.map((rev, i) => (
+          <Reviews review={rev} key={i}/>
+        ))}
+        </div>
+        <p onClick={()=>{
+          setOpenReviewPopup(false);
+        }} className='exit cursor-pointer bold'>X</p>
+
+
+      </div>
     </div>
   )
 }
--- client/src/pages/singleStation/components/StationsRide.jsx
+++ client/src/pages/singleStation/components/StationsRide.jsx
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "../stationRide.css";
 
 import RatingStars from "../../../common/components/RatingStars";
+import ReviewsPopup from "./ReviewsPopup";
 
 const StationsRide = ({stationRide}) => {
+
+  const review = [
+    {
+      id: 1,
+    name: 'JANA HALABI',
+    desc: 'It was a great experience!',
+    rating: 3.5
+  },
+    {
+      id: 2,
+    name: 'JANA HALABI',
+    desc: 'It was a great experience!',
+    rating: 3.5
+  },
+    {
+    id: 3,
+    name: 'JANA HALABI',
+    desc: 'It was a great experience!',
+    rating: 3.5
+  },
+    {
+      id: 4,
+    name: 'JANA HALABI',
+    desc: 'It was a great experience!',
+    rating: 3.5
+  },
+    {
+      id: 5,
+    name: 'JANA HALABI',
+    desc: 'It was a great experience!',
+    rating: 3.5
+  },
+    {
+    id: 6,
+    name: 'JANA HALABI',
+    desc: 'It was a great experience!',
+    rating: 3.5
+  },
+  ]
+    
+
+  const [openReviewPopup, setOpenReviewPopup] = useState(false);
+
+
   return (
     <div className="flex row justify-between w-full bg-black stationRide-card">
+
+      {openReviewPopup && <ReviewsPopup review={review} setOpenReviewPopup={setOpenReviewPopup}/>}
+      
       <div className="flex column gap stationRide-section1">
         <div className="flex row">
           <h3 className="text-primary text-mg-right">From</h3>
@@ -25,7 +73,9 @@ const StationsRide = ({stationRide}) => {
         <RatingStars rating={stationRide.rating} />
       </div>
       <div className="flex column center gap stationRide-section3">
-        <button className="bg-default flex justify-between align-center bold text-white">View Reviews <i className="icon-review"></i></button>
+        <button className="bg-default flex justify-between align-center bold text-white" onClick={()=>{
+          setOpenReviewPopup(true)
+        }}>View Reviews <i className="icon-review"></i></button>
         <button className="bg-default flex justify-between align-center bold text-primary">Buy a Ticket <i className="icon-ticket bg-secondary"></i></button>
       </div>
     </div>
