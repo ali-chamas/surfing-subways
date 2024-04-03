fd42d0b88a8b30070a08892be17e364aef9326ea
watwatos <alichamas.22@hotmail.com>
2024-04-04 02:42:58 +0300

    tet

--- client/src/pages/singleStation/components/BuyTicketPopup.jsx
+++ client/src/pages/singleStation/components/BuyTicketPopup.jsx
@@ -1,6 +1,7 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../../../context/userContext";
-
+import { sendRequest } from "../../../tools/request/request";
+import { ToastContainer, toast } from "react-toastify";
 const BuyTicketPopup = ({
   setOpenBuyTicketPopup,
   station,
@@ -8,77 +9,106 @@ const BuyTicketPopup = ({
   stationRide,
 }) => {
   const { user } = useContext(UserContext);
+  const [type, setType] = useState("one-ride");
+
+  const handlePurchase = async () => {
+    if (user) {
+      const reqBody = { type: type };
+      const res = await sendRequest(
+        "POST",
+        `users/${user.id}/rides/${stationRide.id}/purchase`,
+        reqBody
+      );
+      console.log(res);
+    } else {
+      toast("please login");
+    }
+  };
+
   return (
-    <div className="popup">
-      <div className="flex column align-center popup-child bg-default border-radius gap p">
-        <div className="flex row gap align-center">
-          <h1 className="text-primary letter-spacing">BUY A TICKET</h1>
-          <i className="icon-review-orange"></i>
-        </div>
+    <>
+      <ToastContainer />
+      <div className="popup">
+        <div className="flex column align-center popup-child bg-default border-radius gap p">
+          <div className="flex row gap align-center">
+            <h1 className="text-primary letter-spacing">BUY A TICKET</h1>
+            <i className="icon-review-orange"></i>
+          </div>
 
-        <div className="flex column w-full mg gap">
-          <div className="flex column justify-between w-full bg-black border-radius p">
-            <div>
-              <div className="flex row">
-                <h3 className="text-primary text-mg-right">
-                  Departure Station:
-                </h3>
-                <h3>{station.name}</h3>
+          <div className="flex column w-full mg gap">
+            <div className="flex column justify-between w-full bg-black border-radius p">
+              <div>
+                <div className="flex row">
+                  <h3 className="text-primary text-mg-right">
+                    Departure Station:
+                  </h3>
+                  <h3>{station.name}</h3>
+                </div>
+                <div className="flex row">
+                  <h3 className="text-primary text-mg-right">
+                    Arrival Station:
+                  </h3>
+                  <h3>{arrival}</h3>
+                </div>
+                <div className="flex row">
+                  <h3 className="text-primary text-mg-right">
+                    Departure Time:
+                  </h3>
+                  <h3>{stationRide.departure_time}</h3>
+                </div>
+                <div className="flex row">
+                  <h3 className="text-primary text-mg-right">Arrival Time:</h3>
+                  <h3>{stationRide.arrival_time}</h3>
+                </div>
               </div>
-              <div className="flex row">
-                <h3 className="text-primary text-mg-right">Arrival Station:</h3>
-                <h3>{arrival}</h3>
-              </div>
-              <div className="flex row">
-                <h3 className="text-primary text-mg-right">Departure Time:</h3>
-                <h3>{stationRide.departure_time}</h3>
-              </div>
-              <div className="flex row">
-                <h3 className="text-primary text-mg-right">Arrival Time:</h3>
-                <h3>{stationRide.arrival_time}</h3>
-              </div>
-            </div>
 
-            <div className="flex column align-end w-full">
-              {user && (
-                <>
-                  <h3 className="text-primary">BALANCE:</h3>
-                  <div className="flex row">
-                    <h3>{user.coins}</h3>
-                    <h3 className="text-primary text-mg-right">Coins</h3>
-                  </div>
-                </>
-              )}
-            </div>
-
-            <div className="flex justify-between">
-              <div className="flex row">
-                <h3 className="text-primary text-mg-right">PRICE:</h3>
-                <h3>{stationRide.price}</h3>
-                <h3 className="text-primary text-mg-right">Coins</h3>
+              <div className="flex column align-end w-full">
+                {user && (
+                  <>
+                    <h3 className="text-primary">BALANCE:</h3>
+                    <div className="flex row">
+                      <h3>{user.coins}</h3>
+                      <h3 className="text-primary text-mg-right">Coins</h3>
+                    </div>
+                  </>
+                )}
               </div>
-              <p
-                className="add-rev cursor-pointer bg-secondary text-black bold border-radius flex center mg-top"
-                onClick={() => {
-                  setOpenBuyTicketPopup(false);
-                }}
+              <select
+                onChange={(e) => setType(e.target.value)}
+                vlue={type}
+                className="input"
               >
-                PURCHASE
-              </p>
+                <option value="one-ride">one-ride</option>
+                <option value="multi-rides">multi-rides</option>
+              </select>
+
+              <div className="flex justify-between">
+                <div className="flex row">
+                  <h3 className="text-primary text-mg-right">PRICE:</h3>
+                  <h3>{stationRide.price}</h3>
+                  <h3 className="text-primary text-mg-right">Coins</h3>
+                </div>
+                <p
+                  className="add-rev cursor-pointer bg-secondary text-black bold border-radius flex center mg-top"
+                  onClick={handlePurchase}
+                >
+                  PURCHASE
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <p
-          onClick={() => {
-            setOpenBuyTicketPopup(false);
-          }}
-          className="exit cursor-pointer bold"
-        >
-          X
-        </p>
+          <p
+            onClick={() => {
+              setOpenBuyTicketPopup(false);
+            }}
+            className="exit cursor-pointer bold"
+          >
+            X
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
